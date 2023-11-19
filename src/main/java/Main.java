e2e77954dd3d02d85da33ff280ec6c692428a8e2
nurzamatnasyrkanov <Mu!A3e9e!16!>
2023-11-20 01:05:30 +0600

    checkers board

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+public static void main(String[] args){
+
+    String row8 = "8 | |w| |w| |w| |w|";
+    String row7 = "7 |w| |w| |w| |w| |";
+    String row6 = "6 | |w| |w| |w| |w|";
+    String row5 = "5 | | | | | | | | |";
+    String row4 = "4 | | | | | | | | |";
+    String row3 = "3 |b| |b| |b| |b| |";
+    String row2 = "2 | |b| |b| |b| |b|";
+    String row1 = "1 |b| |b| |b| |b| |";
+    String x =    "   1 2 3 4 5 6 7 8";
+
+    System.out.println(row8);
+    System.out.println(row7);
+    System.out.println(row6);
+    System.out.println(row5);
+    System.out.println(row4);
+    System.out.println(row3);
+    System.out.println(row2);
+    System.out.println(row1);
+    System.out.println(x);
+}
+
+}
