b59de71656809a8ddf0d373b75595609098cd109
nurzamatnasyrkanov <nurzamat@gmail.com>
2023-11-21 20:36:23 +0600

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,24 @@ public static void main(String[] args){
     checkers.init();
     checkers.drawBoard();
 
-    Scanner myObj = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter coordinates in format row1column1 row2column2 to move a figure");
+    String coordinates;
+    while(scanner.hasNextLine()) {
+        coordinates = scanner.nextLine();  // Read user input
+        String[] array = coordinates.split(" ");
+        int iFrom = Integer.parseInt(String.valueOf(array[0].charAt(0))) - 1;
+        int jFrom = Integer.parseInt(String.valueOf(array[0].charAt(1))) - 1;
+        int iTo = Integer.parseInt(String.valueOf(array[1].charAt(0))) - 1;
+        int jTo = Integer.parseInt(String.valueOf(array[1].charAt(1))) - 1;
 
-    String coordinates = myObj.nextLine();  // Read user input
-    String[] array = coordinates.split(" ");
-    int iFrom = Integer.parseInt(String.valueOf(array[0].charAt(0))) - 1;
-    int jFrom = Integer.parseInt(String.valueOf(array[0].charAt(1))) - 1;
-    int iTo = Integer.parseInt(String.valueOf(array[1].charAt(0))) - 1;
-    int jTo = Integer.parseInt(String.valueOf(array[1].charAt(1))) - 1;
-
-    checkers.move(iFrom, jFrom, iTo, jTo);
+        try{
+            checkers.move(iFrom, jFrom, iTo, jTo);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
 
 }
